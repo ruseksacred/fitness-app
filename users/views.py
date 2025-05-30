0b057431def6d22ef3cc56fb60860089bc109e41
Bartosz <brustyn123@gmail.com>
2025-05-30 16:28:58 +0200

    reg fix

--- users/views.py
+++ users/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth import login
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
             return redirect('login')  # Redirect to login page after registration
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
 # Create your views here.
