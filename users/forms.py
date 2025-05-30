ea446cf77f18668ab395af45aa9bbf66c5a52109
Bartosz <brustyn123@gmail.com>
2025-05-30 16:35:47 +0200

    reg fix2

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,10 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
