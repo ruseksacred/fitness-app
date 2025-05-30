4abb5e285ee2883f6cff4d81a9f1f0ab47349132
Bartosz <brustyn123@gmail.com>
2025-05-30 16:56:21 +0200

    xd

--- users/views.py
+++ users/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login
+import logging
 
 def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to login page after registration
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    try:
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')  # Redirect to login page after registration
+        else:
+            form = CustomUserCreationForm()
+        return render(request, 'users/register.html', {'form': form})
+    except Exception as e:
+        logging.exception("Error during user registration: %s", e)
+        
 
 # Create your views here.
