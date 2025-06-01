ecb33aaab598b8606fb6c7601a8a52f91d63691d
Bartosz <brustyn123@gmail.com>
2025-06-01 14:01:56 +0200

    migrations

--- training/models.py
+++ training/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class TrainingSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='training_sessions')
     EXERCISES = [
         ('pushups', 'Pompki'),
         ('situps', 'Brzuszki'),
