b65fbaea353418791f5cb014bb170313ed06b4e6
Bartosz <brustyn123@gmail.com>
2025-06-03 20:59:34 +0200

    dsds

--- training/urls.py
+++ training/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('dodaj/', views.add_training, name='add_training'),
     path('historia/', views.training_history, name='training_history'),
     path('run-migrate/', views.run_migrate),
+    path('show-tables/', views.show_tables),
 ]
